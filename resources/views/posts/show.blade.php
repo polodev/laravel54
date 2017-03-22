2564df1e4b60424b92bd9305be3a22baa88e23d1
polo dev <polodev15@gmail.com>
2017-03-23 00:28:15 +0600

    added file 13

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,5 +4,13 @@
         <h2 class="blog-post-title"> {{ $post->title }} </h2>
         <p class="blog-post-meta">{{ $post->created_at->toFormattedDateString() }}</p>
         <p>{{ $post->body }}</p>
+        <ul class="list-item">
+            @foreach($post->comments as $comment)
+                <li class="list-group-item">
+                    {{ $comment->created_at->diffForHumans() }}
+                    {{ $comment->body }}
+                </li>
+            @endforeach
+        </ul>
     </div>
 @endsection
